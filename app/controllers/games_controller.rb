7da48c52bb5c1b88b01016831e32c87fc1a8ac0f
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-12 00:08:15 +0900

    Added some comments

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,6 +13,7 @@ class GamesController < ApplicationController
     user_input = params[:word].upcase.split('')
     # letters in the grid
     letters = params[:letters].split
+    # validating the word using the wagon dictionary API
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     serialized = open(url).read
     word_json = JSON.parse(serialized)
@@ -22,10 +23,12 @@ class GamesController < ApplicationController
     #   #   # @reply = "Sorry but #{params[:word]} can't be built out of #{@word}"
     #   # end
     # end
+    # checks if the user inputtted letter exists in the given grid
     boolean = user_input.any? do |letter|
       puts letters.include?(letter)
       letters.include?(letter)
     end
+    # validation for the user input
     puts boolean
     if boolean
       if word_json['found']
