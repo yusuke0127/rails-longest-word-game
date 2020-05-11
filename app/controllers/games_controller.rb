a46caaa449776cdbb43f0abab91d82e4196005b3
yusuke ishida <ishidayusuke01@gmail.com>
2020-05-11 19:50:11 +0900

    Implemented longest word game with a web interface using rails, it is a program that gives an random combination of letters that lets the user make a word out of the array. It validates if the letter inputted can be built out of the grid, or if its a valid english word using the lewagon dictionary API

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,41 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @alphabet = Array('A'..'Z')
+    puts @alphabet
+    @letters = Array.new(10) { @alphabet.sample }
+  end
+
+  def score
+    # user input
+    user_input = params[:word].upcase.split('')
+    # letters in the grid
+    letters = params[:letters].split
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    serialized = open(url).read
+    word_json = JSON.parse(serialized)
+    # letter_checker = letters.split.map do |letter|
+    #   # if word.include?(letter)
+    #   #   true
+    #   #   # @reply = "Sorry but #{params[:word]} can't be built out of #{@word}"
+    #   # end
+    # end
+    boolean = user_input.any? do |letter|
+      puts letters.include?(letter)
+      letters.include?(letter)
+    end
+    puts boolean
+    if boolean
+      if word_json['found']
+        @reply = "Valid word"
+      else
+        @reply = "Not a valid word"
+      end
+    else
+      @reply = "Sorry not in the grid"
+    end
+    raise
+  end
+end
